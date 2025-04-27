23db86f3a94f6521e30a7685b4db7da39cbc1473
walkaroundthestars <kamila.przybylska@edu.uekat.pl>
2025-04-27 23:21:00 +0200

    adding reading and saving files, new and old game options

--- main.c
+++ main.c
@@ -99,6 +99,7 @@ void printBoard(int** board, int size, int** originalBoard)
         printf("\n");
         if (i % blockSize == 0 && i != 0)
         {
+            //drawing horizontal line
             drawLine(size*5);
             printf("\n" );
         }
@@ -106,18 +107,62 @@ void printBoard(int** board, int size, int** originalBoard)
         {
             if (j % blockSize == 0 && j != 0)
             {
+                //drawing vertical line
                 printf("|");
             }
             if (board[i][j] != 0 && board[i][j] == originalBoard[i][j]) {
+                //showing already given numbers in green
                 printf("\033[0;32m");
                 printf("%3d  ", board[i][j]);
                 printf("\033[0m");
             }
             else {
+                //showing other numbers and zeros in white
                 printf("%3d  ", board[i][j]);
             }
         }
     }
+}
+
+void saveToFile(int** board, int size) {
+    char fileName[50];
+    sprintf(fileName, "sudoku%dx%d.txt", size, size);
+    FILE* file = fopen(fileName, "w");
+
+    if (file == NULL) {
+        printf("Couldn't open the file.\n");
+        return;
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            fprintf(file, "%d ", board[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+    printf("File saved as %s.\n", fileName);
+}
+
+void readFromFile(int** board, int size) {
+    char fileName[50];
+    sprintf(fileName, "sudoku%dx%d.txt", size, size);
+    FILE* file = fopen(fileName, "r");
+
+    if (file == NULL) {
+        printf("Couldn't open the file.\n");
+        return;
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            fscanf(file, "%d", &board[i][j]);
+        }
+    }
+
+    fclose(file);
+    printf("Game loaded.\n");
 }
 
 //checking if game is over
@@ -129,7 +174,7 @@ int isBoardComplete(int** board, int size) {
     return 1;
 }
 
-//function for copying board (to prevent making changes in original board)
+//function for copying board (to prevent from making changes in original board)
 void copyBoard(int** board, int** originalBoard, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -153,66 +198,8 @@ void removeNumbers(int** board, int size, int quantityToDelete) {
     }
 }
 
-int main(void) {
-    int mistakes = 0;
-    int size;
-
-    printf("Choose size of board:\n1) 4x4\n2) 9x9\n3) 16x16\n4) 25x25\n");
-    int answer;
-    scanf("%d", &answer);
-
-    while (answer != 1 && answer != 2 && answer != 3 && answer != 4) {
-        printf("Please enter correct value.\n");
-        scanf("%d", &answer);
-    }
-
-    if (answer == 1) {
-        size = 4;
-    }
-    else if (answer == 2) {
-        size = 9;
-    }
-    else if (answer == 3) {
-        size = 16;
-    }
-    else if (answer == 4) {
-        size = 25;
-    }
-
-
-    int **board = malloc(size * sizeof(int*));
-    for (int i = 0; i < size; i++) {
-        board[i] = malloc(size * sizeof(int));
-    }
-
-    printf("Choose level: 1, 2 or 3:\n");
-    int level;
-    scanf("%d", &level);
-
-    while (level != 1 && level != 2 && level != 3) {
-        printf("Please choose correct option.\n");
-        scanf("%d", &level);
-    }
-
-    createBoard(board, size);
-
-
-    if (level == 1) {
-        removeNumbers(board, size, (int)(size * size *  0.4));
-    }
-    else if (level == 2) {
-        removeNumbers(board, size, (int)(size * size *  0.6));
-    }
-    else if (level == 3) {
-        removeNumbers(board, size, (int)(size * size *  0.8));
-    }
-
-    int **originalBoard = malloc(size * sizeof(int*));
-    for (int i = 0; i < size; i++) {
-        originalBoard[i] = malloc(size * sizeof(int));
-    }
-    copyBoard(board, originalBoard, size);
-
+//loop of the game
+void game(int** board, int size, int** originalBoard, int mistakes) {
     while (1) {
         printBoard(board, size, originalBoard);
         if (isBoardComplete(board, size) == 1) {
@@ -229,7 +216,18 @@ int main(void) {
             break;
         }
         if (row == -1) {
-            break;
+            while (getchar() != '\n');
+            printf("Do you want to save the game? y/n\n");
+            char ans;
+            ans = getchar();
+            if (ans == 'n') {
+                break;
+            }
+            if (ans == 'y') {
+                printf("savingggg....");
+                saveToFile(board, size);
+                break;
+            }
         }
         if (scanf("%d %d", &col, &value) != 2) {
             break;
@@ -245,13 +243,80 @@ int main(void) {
             printf("You can't change the numbers that are already given.\n");
         }
         else if (isValidMove(board, size, row, col, value) == 0) {
+            printf("\033[0;31m");
             printf("Invalid move.\n");
+            printf("\033[0m");
             mistakes++;
         }
         else {
             board[row][col] = value;
         }
     }
+}
+
+//creating new game
+void newGame() {
+    int mistakes = 0;
+    int size;
+
+    //choose of size of board
+    printf("Choose size of board:\n1) 4x4\n2) 9x9\n3) 16x16\n");
+    int answer;
+    scanf("%d", &answer);
+
+    while (answer != 1 && answer != 2 && answer != 3) {
+        printf("Please enter correct value.\n");
+        scanf("%d", &answer);
+    }
+
+    if (answer == 1) {
+        size = 4;
+    }
+    else if (answer == 2) {
+        size = 9;
+    }
+    else if (answer == 3) {
+        size = 16;
+    }
+
+    int **board = malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        board[i] = malloc(size * sizeof(int));
+    }
+
+    //choose of difficulty level
+    printf("Choose level: \n1) Easy\n2) Medium\n3) Hard\n4) Super hard\n");
+    int level;
+    scanf("%d", &level);
+
+    while (level != 1 && level != 2 && level != 3 && level != 4) {
+        printf("Please choose correct option.\n");
+        scanf("%d", &level);
+    }
+
+    createBoard(board, size);
+
+
+    if (level == 1) {
+        removeNumbers(board, size, (int)(size * size *  0.4));
+    }
+    else if (level == 2) {
+        removeNumbers(board, size, (int)(size * size *  0.6));
+    }
+    else if (level == 3) {
+        removeNumbers(board, size, (int)(size * size *  0.8));
+    }
+    else if (level == 4) {
+        removeNumbers(board, size, (int)(size * size *  0.95));
+    }
+
+    int **originalBoard = malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        originalBoard[i] = malloc(size * sizeof(int));
+    }
+    copyBoard(board, originalBoard, size);
+
+    game(board, size, originalBoard, mistakes);
 
     for (int i = 0; i < size; i++) {
         free(board[i]);
@@ -259,5 +324,95 @@ int main(void) {
     }
     free(board);
     free(originalBoard);
+}
+
+//loading and preparing old game
+void oldGame() {
+    int size;
+
+    printf("What size of game should I look for?\n1) 4x4\n2) 9x9\n3) 16x16\n");
+    int answer;
+
+    scanf("%d", &answer);
+    while (answer != 1 && answer != 2 && answer != 3) {
+        printf("Please choose correct option.\n");
+        scanf("%d", &answer);
+    }
+
+    if (answer == 1) {
+        size = 4;
+    }
+    else if (answer == 2) {
+        size = 9;
+    }
+    else if (answer == 3) {
+        size = 16;
+    }
+
+    int **board = malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        board[i] = malloc(size * sizeof(int));
+    }
+
+    //load from file
+    //load and save mistakes
+    readFromFile(board, size);
+
+    int **originalBoard = malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        originalBoard[i] = malloc(size * sizeof(int));
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            originalBoard[i][j] = 0;
+        }
+    }
+
+    game(board, size, originalBoard, 0);
+
+    for (int i = 0; i < size; i++) {
+        free(board[i]);
+        free(originalBoard[i]);
+    }
+    free(board);
+    free(originalBoard);
+}
+
+int main(void) {
+
+    printf("Choose option from menu:\n1) New game\n2) Old game\n3) How to play\n 4) Exit\n");
+    int menu;
+    scanf("%d", &menu);
+
+    while (menu != 1 && menu != 2 && menu != 3 && menu != 4) {
+        printf("Please enter correct value.\n");
+        scanf("%d", &menu);
+    }
+
+    if (menu == 1) {
+        newGame();
+    }
+    else if (menu == 2) {
+        oldGame();
+    }
+    else if (menu == 3) {
+        printf("\n*** How to Play Sudoku ***\n");
+        printf("1. Choose the board size and difficulty level.\n");
+        printf("2. Fill every empty cell so that:\n");
+        printf("   - Each number appears once in every row,\n");
+        printf("   - Each number appears once in every column,\n");
+        printf("   - Each number appears once in every box.\n");
+        printf("3. To make a move, enter three numbers:\n");
+        printf("   (row) (column) (value)\n");
+        printf("   Example: 2 3 5 (puts 5 in row 2, column 3)\n");
+        printf("4. You cannot change the given numbers.\n");
+        printf("5. Wrong moves will be counted as mistakes.\n");
+        printf("6. Enter -1 as the row number to quit the game.\n\n");
+    }
+    else if (menu == 4) {
+        printf("Thank you for playing!");
+    }
+
     return 0;
 }
